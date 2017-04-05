ad98c664e74565527a9fe27c13a977b5813b18a5
Abdullah Al Mahmud <mahmudcuet10@gmail.com>
2017-04-05 20:14:16 +0600

    add form for image upload

--- server/server.js
+++ server/server.js
@@ -5,9 +5,41 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname +'/pictures/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, req.params.id + path.extname(file.originalname));
+  }
+})
+
+var upload = multer({ storage: storage })
 
 var app = module.exports = loopback();
+app.middleware('initial', bodyParser.urlencoded({ extended: true }));
 
+app.post('/api/cars/:id/image', upload.single('image'), function(req, res) {
+console.log('here ',req.params.id);
+res.send('perfectly ok!');
+  // app.models.car.findById(req.params.id, function(err, car) {
+  //   if(err) return res.sendStatus(400);
+  //   else {
+  //     car.imageUrl = req.file.filename;
+  //     car.save(function(err, car){
+  //       if(err) return res.sendStatus(400);
+  //       else return res.sendStatus(200);
+  //     });
+  //   }
+  // });
+  
+})
 app.start = function() {
   // start the web server
   return app.listen(function() {
